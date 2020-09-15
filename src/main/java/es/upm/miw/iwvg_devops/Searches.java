2f42ba1c6eba006c2d5a470bb3271fc09c8fc72e
Pablo <pabloguijarro96@gmail.com>
2020-09-15 10:24:17 +0200

    Comment prove6

--- src/main/java/es/upm/miw/iwvg_devops/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/Searches.java
@@ -120,7 +120,7 @@ public class Searches {
         //return Stream.empty();
     }
 
-    public Stream<String> findUserFamilyNameByAllNegativeSignFractionDistinct() {//Prueba5Git
+    public Stream<String> findUserFamilyNameByAllNegativeSignFractionDistinct() {//Prueba6Git
         return Stream.empty();
     }
 
