b845a89adcd91761ed5f565058ef4f872d4eda92
Pablo <pabloguijarro96@gmail.com>
2020-09-15 10:11:48 +0200

    Comment prove

--- src/main/java/es/upm/miw/iwvg_devops/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/Searches.java
@@ -120,7 +120,7 @@ public class Searches {
         //return Stream.empty();
     }
 
-    public Stream<String> findUserFamilyNameByAllNegativeSignFractionDistinct() {
+    public Stream<String> findUserFamilyNameByAllNegativeSignFractionDistinct() {//Prueba1Git
         return Stream.empty();
     }
 
